3c8a5f5894c7de7eae31f7079a0aaf218d0dc571
YahyaKhan <yahya.b.khan@gmail.com>
2024-10-10 18:39:12 -0400

    Minor Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -24,11 +24,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> Site in Progress
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Donate and AzadCharityFoundation icons to learn more
       </p>
 
       <Routes>
